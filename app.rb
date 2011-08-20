5053b0a13b4d6f36ff9dc9d913395db58cf0a9c0
Chad Jolly <cjolly@lawgical.com>
2011-08-20 15:58:30 -0600

    redirect heroku domain

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ set :haml, {:format => :html5, :ugly => settings.environment == :production}
 set :sass, {:cache_location => 'tmp/sass-cache'}
 
 before do
-  redirect to('http://chadjolly.com'), 301 if request.host == 'jollycomputers.com'
+  redirect to('http://chadjolly.com'), 301 if request.host == /^jollycomputers.com|heroku.com$/
 end
 
 get '/' do

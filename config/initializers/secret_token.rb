0911ccf4468cc409df9de8d23b13524c96fe134d
Chad Jolly <cjolly@lawgical.com>
2011-10-15 22:10:36 -0600

    Rails 3.1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chadjolly::Application.config.secret_token = 'af7a095718875ee04fc7c1771e26e4cac865bbd4199fafc665d7878d34b3c4eb69650c4fd19ec57f1f1692763e355c6bab2db970ce51944dbf246df16ab495c6'

20c61e8638036f2119bea3787dbff4a92dac4c3b
Chad Jolly <cjolly@lawgical.com>
2012-09-06 11:57:48 -0600

    use uuid in log tags

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,7 @@ ChadJolly::Application.configure do
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
+  config.log_tags = [:uuid]
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new

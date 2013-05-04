f5dc0b7010bd45a41b0bf31a529bdd21bd0a5441
Chad Jolly <cjolly@lawgical.com>
2013-05-04 17:37:30 -0600

    tmp workaround to serve static assets until heroku fixes buildpack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,9 @@ ChadJolly::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  # TODO: workaround until heroku buildpack is released with support:
+  # https://github.com/heroku/heroku-buildpack-ruby/pull/74
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier

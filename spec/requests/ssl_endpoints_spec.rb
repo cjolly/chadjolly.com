c393edd76933d64676b70cc49da58b7bc1e45421
Chad Jolly <cjolly@lawgical.com>
2016-09-10 19:55:06 -0600

    Rails 4.2

--- spec/requests/ssl_endpoints_spec.rb
+++ spec/requests/ssl_endpoints_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 # ENV vars set in spec/spec_helper.rb
 RSpec.describe "Let's Encrypt SSL challenge routes", type: :request do
   it "responds with private ENV var based on shared token" do
-    get ".well-known/acme-challenge/wile-e"
+    get "/.well-known/acme-challenge/wile-e"
     expect(response.body).to eq('coyote')
   end
 
   it "does not route when incorrect token provided" do
     expect {
-      get ".well-known/acme-challenge/meep-meep"
+      get "/.well-known/acme-challenge/meep-meep"
     }.to raise_error(ActionController::RoutingError)
   end
 end

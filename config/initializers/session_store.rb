d4a40d24062cf339abb868d7cf5a99f79e7cd868
Chad Jolly <cjolly@lawgical.com>
2016-09-10 20:24:04 -0600

    Rails 4.2 `rake rails:upgrade`

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ChadJolly::Application.config.session_store :cookie_store, key: '_chadjolly_session'
+Rails.application.config.session_store :cookie_store, key: '_chad_jolly_session'

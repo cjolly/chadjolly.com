c88bb3a2ba29993aeed9608c2563a0d530a8ab64
Chad Jolly <cjolly@lawgical.com>
2018-09-25 20:59:46 -0600

    move to heroku's built in lets encrypt support

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,6 @@ ChadJolly::Application.routes.draw do
   get '/errors', to: 'pages#errors'
   resources :passes
 
-  # Let's Encrypt!
-  # https://github.com/dmathieu/sabayon
-  ENV.each do |var, _|
-    next unless var.start_with?("ACME_TOKEN_")
-    number = var.sub(/ACME_TOKEN_/, '')
-    get ".well-known/acme-challenge/#{ ENV["ACME_TOKEN_#{number}"] }" => proc { [200, {}, [ ENV["ACME_KEY_#{number}"] ] ] }
-  end
-
   # Stop the noisy rails logging for missed routes.
   get '/*all' => proc {|env| [404, {}, [] ]}, via: :all
 end
